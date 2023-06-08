ec3ef0f58bf299a71600b8088153aa10a0ca2288
Wahyu Harry Saputra Sembiring <106479145+wahyusaputra073@users.noreply.github.com>
2023-06-09 05:30:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from jinja2 import Environment, FileSystemLoader
+import mysql.connector
 from sklearn.cluster import DBSCAN, KMeans
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    # if request.method == 'POST':
+    return tampil_halaman(1)
+
+@app.route('/halaman/<int:halaman>')
+def tampil_halaman(halaman):
+    JUMLAH_PER_HALAMAN = 15
 
     # Load Data
     data_cluster = pd.read_csv("https://storage.googleapis.com/tim_panel1/DataCobaNew.csv")
@@ -25,6 +33,7 @@ def index():
     kmeans = KMeans(n_clusters=5, random_state=42).fit(data_cluster_std)
     labels = kmeans.labels_
     
+
     # Add labels to a new column
     ikan = pd.read_csv("https://storage.googleapis.com/tim_panel1/DataCobaNew.csv")
     ikan['Cluster'] = labels
@@ -50,27 +59,44 @@ def index():
     search_query = request.args.get('search')
     kota_query = request.args.get('kota')
     
+    if search_query and kota_query:
+        outputkab = ikan[
+        (ikan['Jenis_Ikan'].str.contains(search_query, case=False, na=False) | ikan['KabKota'].str.contains(search_query, case=False)) &
+        ikan['KabKota'].str.contains(kota_query, case=False, na=False)
+    ] 
+
     if search_query:
         outputkab = ikan[
-            ikan['Jenis_Ikan'].str.contains(search_query, case=False) |
-            ikan['KabKota'].str.contains(search_query, case=False)
+            (ikan['Jenis_Ikan'].str.contains(search_query, case=False, na=False)) &
+            (ikan['KabKota'].str.contains(kota_query, case=False, na=False))
         ]
     elif kota_query:
-        outputkab = ikan[ikan['KabKota'].str.contains(kota_query, case=False)]
+        outputkab = ikan[ikan['KabKota'].str.contains(kota_query, case=False, na=False)]
     else:
         outputkab = ikan.copy()
+       
+    # Apply pagination
+    awal = (halaman-1) * JUMLAH_PER_HALAMAN
+    akhir = awal + JUMLAH_PER_HALAMAN
+    outputkab = outputkab[awal:akhir]
+
+    return render_template(
+        'index.html', 
+        table=outputkab.to_dict(orient='records'), 
+        page=halaman, 
+        JUMLAH_PER_HALAMAN=JUMLAH_PER_HALAMAN,
+        search_query=search_query, kota_query=kota_query)
 
 
-    # Take one Region
-    outputkab = outputkab[['KabKota', 'Jenis_Ikan', 'Status']].copy()
-    
-    # Limit the output to a maximum of 10 rows
-    outputkab = outputkab.head(100)    
-    
-    
-#     =================================================================================================
 
 
+@app.route('/result', methods=['GET', 'POST'])
+def index2():
+    return tampil_halaman2(1)
+
+@app.route('/halaman2/<int:halaman2>', methods=['GET', 'POST'])
+def tampil_halaman2(halaman2):
+    JUMLAH_PER_HALAMAN = 15
     # Melakukan proses clustering
     df = pd.read_csv("https://storage.googleapis.com/tim_panel1/DataCobaNew.csv")
     df = df[['Volume_Produksi', 'Konsumsi']]
@@ -102,46 +128,63 @@ def index():
         (ikan['Cluster'] == 3),
         (ikan['Cluster'] == 4)
     ]
-    values = [
-        'Boleh untuk ditangkap',
-        'Sehat untuk ditangkap',
-        'Jumlah terbatas',
-        'Overfishing',
-        'Aman'
-    ]
+    
+    values = ['Sehat untuk ditangkap', 
+              'Lebih baik dibatasi',
+              'Overfishing',
+              'Boleh dilebihkan',
+              'Aman']
     ikan['Status'] = np.select(condition, values)
 
     # Sort custom
     urutan = [
-        (ikan['Status'] == 'Overfishing'),
-        (ikan['Status'] == 'Sehat untuk ditangkap'),
-        (ikan['Status'] == 'Overfishing'),
-        (ikan['Status'] == 'Jumlah terbatas')
-    ]
-    values = [1, 2, 3, 4]
+            (ikan['Status'] == 'Sehat untuk ditangkap'),
+            (ikan['Status'] == 'Lebih baik dibatasi'),
+            (ikan['Status'] == 'Overfishing'),
+            (ikan['Status'] == 'Boleh dilebihkan'),
+            (ikan['Status'] == 'Aman'),
+            ]
+    values = [1, 2, 3, 4, 5]
     ikan['temp'] = np.select(urutan, values)
     ikan = ikan.sort_values(by=['temp'])
 
     # Filter by search query
     search_query = request.args.get('search')
     kota_query = request.args.get('kota')
-    
+
+
+    if search_query and kota_query:
+        outputkab2 = ikan[
+        (ikan['Jenis_Ikan'].str.contains(search_query, case=False, na=False) | ikan['KabKota'].str.contains(search_query, case=False)) &
+        ikan['KabKota'].str.contains(kota_query, case=False, na=False)
+    ]
+
     if search_query:
         outputkab2 = ikan[
-            ikan['Jenis_Ikan'].str.contains(search_query, case=False) |
-            ikan['KabKota'].str.contains(search_query, case=False)
+            (ikan['Jenis_Ikan'].str.contains(search_query, case=False, na=False)) &
+            (ikan['KabKota'].str.contains(kota_query, case=False, na=False))
         ]
     elif kota_query:
-        outputkab2 = ikan[ikan['KabKota'].str.contains(kota_query, case=False)]
+        outputkab2 = ikan[ikan['KabKota'].str.contains(kota_query, case=False, na=False)]
     else:
         outputkab2 = ikan.copy()
-
-        outputkab2 = outputkab[['KabKota', 'Jenis_Ikan', 'Status']].copy()   
+        
+    outputkab2 = outputkab2[['KabKota', 'Jenis_Ikan', 'Status']].copy()   
     
-    # Display
-    outputkab2 = outputkab2.head(10)
 
-    return render_template('index.html', outputkab=outputkab, outputkab2=outputkab2)
+    # Display
+    outputkab2 = outputkab2.head(70)   
+    
+    awal = (halaman2-1) * JUMLAH_PER_HALAMAN
+    akhir = awal + JUMLAH_PER_HALAMAN
+    outputkab2 = outputkab2[awal:akhir]   
+
+    return render_template(
+        'index2.html', 
+        table=outputkab2.to_dict(orient='records'), 
+        page=halaman2, 
+        JUMLAH_PER_HALAMAN=JUMLAH_PER_HALAMAN)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
